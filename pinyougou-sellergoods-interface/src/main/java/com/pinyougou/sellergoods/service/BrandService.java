921d3097ece4eea99639fc29f2bd0611725fa43b
18447 <1844704364@qq.com>
2023-10-25 00:31:14 +0800

    day2

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,6 +1,7 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
@@ -10,4 +11,17 @@ import java.util.List;
  */
 public interface BrandService {
     public List<TbBrand> findAll();
+
+    public PageResult findPage(int pageNum,int pageSize);
+
+//    增加
+    public void add(TbBrand brand);
+    //修改
+    public void update(TbBrand brand);
+    /*根据id获取实体*/
+    public TbBrand findOne(Long id);
+    /*批量删除*/
+    public void delete(Long [] ids);
+    /*分页*/
+    public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
 }
