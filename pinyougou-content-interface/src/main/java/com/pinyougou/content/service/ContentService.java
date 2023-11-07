8db76d85c545a67966844a8f8505cdb0dc7d9e87
18447 <1844704364@qq.com>
2023-11-07 21:32:03 +0800

    day08-轮播图完成

--- pinyougou-content-interface/src/main/java/com/pinyougou/content/service/ContentService.java
+++ pinyougou-content-interface/src/main/java/com/pinyougou/content/service/ContentService.java
@@ -57,5 +57,11 @@ public interface ContentService {
 	 * @return
 	 */
 	public PageResult findPage(TbContent content, int pageNum,int pageSize);
-	
+
+	/*
+	* 根据广告类型ID查询列表
+	*
+	* */
+	public List<TbContent> findByContentCategoryId(Long categoryId);
+
 }
